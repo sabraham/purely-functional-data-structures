e3b480b5a68a3e3a18307866015ae34c55e75e32
aki <spacemanaki@gmail.com>
2013-06-28 13:58:35 -0400

    just an alternative ORDERED sig for comparison
    I know that the OCaml standard lib uses a compare function with type
    t * t -> int, and it looks like Jane Street's lib replacement does
    too, but I much prefer this, which feels much more MLish to me than
    checking the sign of an int.
    
    Were I actually writing an OCaml app I would probably just put up with
    the version returning an int, but couldn't resist in this scenario ;)
    
    http://caml.inria.fr/pub/docs/manual-ocaml/libref/Set.OrderedType.html
    https://ocaml.janestreet.com/ocaml-core/latest/doc/core_kernel/Comparator.html#TYPEt
    
    http://www.smlnj.org/doc/smlnj-lib/Manual/ord-key.html#ORD_KEY:SIG:SPEC

--- ch-2.ml
+++ ch-2.ml
@@ -82,10 +82,12 @@ let suffixes xs =
 
 (* ORDERED Signature *)
 
+type order = Less | Greater | Equal
+
 module type ORDERED =
 sig
   type t
-  val compare : t -> t -> int
+  val compare : t -> t -> order
 end
 
 (* SET Signature *)
@@ -112,17 +114,17 @@ struct
         EmptyTree -> Node(EmptyTree, x, EmptyTree)
       | Node(left, k, right) ->
         match Element.compare x k with
-            0 -> s
-          | n when n < 0 -> Node(insert x left, k, right)
-          | _ -> Node(left, k, insert x right)
+            Equal   -> s
+          | Less    -> Node(insert x left, k, right)
+          | Greater -> Node(left, k, insert x right)
   let rec member x s =
     match s with
         EmptyTree -> false
       | Node(left, k, right) ->
         match Element.compare x k with
-            0 -> true
-          | n when n < 0 -> member x left
-          | _ -> member x right
+            Equal   -> true
+          | Less    -> member x left
+          | Greater -> member x right
 end
 
 (* Exercise 2.2 *)
@@ -138,9 +140,9 @@ struct
         EmptyTree -> Node(EmptyTree, x, EmptyTree)
       | Node(left, k, right) ->
         match Element.compare x k with
-          0            -> s
-        | n when n < 0 -> Node(insert x left, k, right)
-        | _            -> Node(left, k, insert x right)
+          Equal   -> s
+        | Less    -> Node(insert x left, k, right)
+        | Greater -> Node(left, k, insert x right)
 
   let rec member x s =
     let rec member' x s cand =
@@ -148,12 +150,12 @@ struct
           x, EmptyTree -> if x = cand then true else false
         | x, Node(left, k, right) ->
           match Element.compare x k with
-            n when n < 0 -> member' x left cand
-          | _            -> member' x right k
+            Less -> member' x left cand
+          | _    -> member' x right k
     in match x, s with
         _, EmptyTree -> false
       | x, Node(left, k, right) ->
         match Element.compare x k with
-          n when n < 0 -> member x left
-        | _            -> member' x right k
+          Less -> member x left
+        | _    -> member' x right k
 end
